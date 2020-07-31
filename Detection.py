b8458e8d714140401993e2064b0b757cc32c6e7b
Gaurav Yadav <gauravyadav72201@gmail.com>
2020-08-01 00:02:14 +0530

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -15,6 +15,8 @@ dataset = pd.read_csv('wdbc.csv')
 X = dataset.iloc[:, 1:31].values
 Y = dataset.iloc[:, 31].values
 
+
+#Dataset visualization 
 dataset.describe()
 
 dataset.head(5)
@@ -24,7 +26,6 @@ dataset.shape
 dataset.info()
 
 
-dataframe = pd.DataFrame(Y)
 #Encoding categorical data values 
 from sklearn.preprocessing import LabelEncoder
 labelencoder_Y = LabelEncoder()
@@ -46,43 +47,43 @@ X_test = sc.transform(X_test)
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, Y_train)
-#97.183 Acuracy
+
 
 #Fitting K-NN Algorithm
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors = 3, metric = 'minkowski', p = 2)
 classifier.fit(X_train, Y_train)
-#95.774 Acuracy
+
 
 #Fitting SVM
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, Y_train) 
-#95.77 Acuracy
+
 
 #Fitting K-SVM
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, Y_train)
-#97.183 Acuracy
+
 
 #Fitting Naive_Bayes
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, Y_train)
-#93.66 Acuracy
+
 
 #Fitting Decision Tree Algorithm
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, Y_train)
-#90.14 Acuracy
+
 
 #Fitting Random Forest Classification Algorithm
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 1000, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, Y_train)
-#95.774 Acuracy
+
 
 #predicting the Test set results
 Y_pred = classifier.predict(X_test)
